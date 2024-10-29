6c7b865af4736f9a08556a80a45c846686b5aa22
Ashif Mujtoba <nmujtoba@gmail.com>
2024-10-30 02:40:04 +0600

    class 03 done

---
+++ 01 Introduction & Dart Basics/Class 03 - Flutter Project Creation & Dart/lib/dart_basics.dart
@@ -0,0 +1,36 @@
+//DART
+
+//create a file .dart format
+//write main function
+
+void main() {
+//Now write code to see output
+
+//1. Keywords
+//2. Variables
+//3. Data Types
+
+//Variables (container/identifier)
+
+//Using var
+
+  var name = 'Ashif'; //text data
+  name = 'Shuvro';
+
+  var age = 32;
+
+//   final lastName = 'Mujtoba';
+
+  //instant/ compile time value
+  const pi = 3.14;
+
+  //Run time value
+  final lastName;
+
+  lastName = 'Mujtoba';
+
+  print(name);
+  print(age);
+  print(pi);
+  print(lastName);
+}
